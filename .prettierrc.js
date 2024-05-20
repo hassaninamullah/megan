8cea01b4691d0f85f22f9d86d477955bb3989b91
Hassan <hassaninamullah@gmail.com>
2024-05-20 16:12:49 +0500

    Add files via upload

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    arrowParens: 'avoid',
+    tabWidth: 2,
+    semi: true,
+    printWidth: 9999999999,
+};
